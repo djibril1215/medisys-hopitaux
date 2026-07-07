901319b83553bafc313d124bfd58cdadd95a9765
djibril1215 <pierredjibrilgamani@gmail.com>
2026-07-07 02:44:55 +0100

    feat: refonte du cycle de transfert - reserve aux medecins, isolation multi-tenant, creation reelle de dossier avec choix continuer/nouveau

--- src/routes/hopitalRoutes.js
+++ src/routes/hopitalRoutes.js
@@ -5,15 +5,19 @@ const {
   createTransfert, getAllTransferts, validerTransfert,
 } = require('../controllers/hopitalController');
 const verifyToken = require('../middlewares/authMiddleware');
+const requireRole = require('../middlewares/requireRole');
 
-router.post('/', verifyToken, createHopital);
 router.get('/', verifyToken, getAllHopitaux);
 router.get('/:id', verifyToken, getHopitalById);
-router.put('/:id', verifyToken, updateHopital);
-router.delete('/:id', verifyToken, deleteHopital);
 
-router.post('/transferts', verifyToken, createTransfert);
+// Gestion du reseau d'hopitaux - reservee au super_admin
+router.post('/', verifyToken, requireRole(['super_admin']), createHopital);
+router.put('/:id', verifyToken, requireRole(['super_admin']), updateHopital);
+router.delete('/:id', verifyToken, requireRole(['super_admin']), deleteHopital);
+
+// Transferts - reserves aux medecins
+router.post('/transferts', verifyToken, requireRole(['medecin']), createTransfert);
 router.get('/transferts/all', verifyToken, getAllTransferts);
-router.patch('/transferts/:id/valider', verifyToken, validerTransfert);
+router.patch('/transferts/:id/valider', verifyToken, requireRole(['medecin']), validerTransfert);
 
 module.exports = router;
